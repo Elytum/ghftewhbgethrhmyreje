4549d2f1b33ceb9a6d1ee678481addbf35bcb73c
Arthur CHAZAL <achazal@e2r1p11.42.fr>
2016-05-19 01:20:55 +0200

    Email handling ok

--- public/doreset.php
+++ public/doreset.php
@@ -4,9 +4,9 @@
 	<body>
 		<form action="call_reset.php" method="get">
 			New password:
-			<input type="text" name="newpassword" value="" class="formular" />
-			<input hidden="true" type="text" name="email" value=<?php if (array_key_exists ('email' , $_GET) == false) echo "\"\"" else echo "\"".$_GET['email']."\""?> class="formular" />
-			<input hidden="true" type="text" name="token" value=<?php if (array_key_exists ('token' , $_GET) == false) echo "\"\"" else echo "\"".$_GET['token']."\""?> class="formular" />
+			<input type="password" name="newpassword" value="" class="formular" />
+			<input hidden="true" type="text" name="email" value=<?php if (array_key_exists ('email' , $_GET) == false) echo "\"\""; else echo "\"".$_GET['email']."\""?> class="formular" />
+			<input hidden="true" type="text" name="token" value=<?php if (array_key_exists ('token' , $_GET) == false) echo "\"\""; else echo "\"".$_GET['token']."\""?> class="formular" />
 			<input name="submit" type="submit" value="Submit">
 		</form>
 	</body>
--- public/validate.php
+++ public/validate.php
@@ -3,6 +3,31 @@
 		return ;
 	$email = $_GET['email'];
 	$token = $_GET['token'];
-	echo $email;
-	echo $token;
+	
+	$servername = "127.0.0.1";
+	$username = "root";
+	$pass = "";
+	$port = "8081";
+	$dbname = "camagruDB";
+	try {
+		$conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $pass, array( PDO::ATTR_PERSISTENT => true));
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$account_exist = $conn->prepare('SELECT COUNT(*) FROM users WHERE email=? AND ready=0;');
+		$account_exist->bindParam(1, $email);
+		$account_exist->execute();
+		$result = $account_exist->fetchAll();
+		if ($result[0][0] == 0)
+			return ;
+
+		$ready_user = $conn->prepare('UPDATE users SET ready=1 WHERE email=?;');
+		$ready_user->bindParam(1, $email);
+		$ready_user->execute();
+		header('Location: /index.php');
+		exit();
+	}
+	catch(PDOException $e)
+	{
+		echo "Error: Unknown";//Connection failed on subscribe: " . $e->getMessage();
+	}
 ?>
